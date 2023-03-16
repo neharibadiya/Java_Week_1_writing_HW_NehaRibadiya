f764f7d30c6c114638dd8dd71cf27c39f31f30a4
neharibadiya <nehagajera25@gmail.com>
2023-03-16 08:15:14 +0000

    my week 1 writing homework

---
+++ src/homework_week_5/Programme_10_ProductOfTwoNumbers.java
@@ -0,0 +1,13 @@
+package homework_week_5;
+
+public class Programme_10_ProductOfTwoNumbers {
+    public static void main(String[] args) {
+        //first number declaration
+        int firstNumber = 25;
+        //second number declaration
+        int secondNumber = 5;
+        int produced = firstNumber * secondNumber;
+        //Expected output
+        System.out.println(firstNumber +" x " + secondNumber+" = " +produced);
+    }
+}
---
+++ src/homework_week_5/Programme_1_BioData.java
@@ -0,0 +1,23 @@
+package homework_week_5;
+
+/**
+ * 1.write a programme that print your Bio Data.
+ */
+public class Programme_1_BioData {
+    public static void main(String[] args) {
+        System.out.println("            BIO DATA            ");
+        System.out.println("Name            : jane smith");
+        System.out.println("Mobile          : 07956 832458");
+        System.out.println("Email           : janesmith@gmail.com");
+        System.out.println("Gender          : Female");
+        System.out.println("Date Of Birth   : 01.06.2001");
+        System.out.println("Matital status  : single");
+        System.out.println(" Language       : English, Spanish and German");
+        System.out.println("Oaulification   : Software Tester");
+        System.out.println("Experiance      : Agile Tester with 5 years of experiance");
+        System.out.println("Address         : 123 abc sterret london AB3 4GT");
+        System.out.println("Place           : London");
+        System.out.println("Date            : 02.10.2022");
+        System.out.println("signature       : Jsmith");
+    }
+}
